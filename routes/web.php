d1a150af277ae1a5cbf7fe60487bbb359b10b08d
Daniel Medeiros <danielmmf@gmail.com>
2018-12-04 02:12:15 -0200

    client cardapio

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,7 @@ $router->get('/', function () use ($router) {
 $router->get('/client/', function () use ($router) {
     return view('client/index', ['app_name' => 'app de teste' , 'public' => '/adm/']);
 });
+
+$router->get('/client/cardapio', function () use ($router) {
+    return view('client/cardapio', ['app_name' => 'app de teste' , 'public' => '/adm/']);
+});
