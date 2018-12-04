5a1cd28148a1c14ee6d8edce893ce0b43cefc95a
Daniel Medeiros <danielmmf@gmail.com>
2018-12-03 22:20:50 -0200

    layout de client

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 $router->get('/', function () use ($router) {
     echo "Benjamin Lab";
 });
+
+$router->get('/client/', function () use ($router) {
+    return view('client/index', ['app_name' => 'app de teste' , 'public' => '/adm/']);
+});
