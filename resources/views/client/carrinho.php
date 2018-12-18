3b166d73fde050584f814ce8a3d820827f50eb28
Leticia Pimenta <13leticia.pimentas@gmail.com>
2018-12-18 12:50:56 -0200

    modal carrinho-collapse

--- resources/views/client/carrinho.php
+++ resources/views/client/carrinho.php
@@ -503,45 +503,89 @@ $scope.carrinhos = carts_atualizado;
           </div>
           <div class="modal-body">
             <form class="form" method="" action="" autocomplete="on">
-              <p class="description text-center">Pagamento com cartão de crédito</p>
+              <p class="description text-center">Formas de pagamento</p>
               <div class="card-body">
-                <div class="form-group bmd-form-group">
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text">
-                        <i class="material-icons">face</i>
-                      </span>
-                    </div>
-                    <input type="text" class="form-control" placeholder="First Name..." autocomplete="on">
-                  </div>
-                </div>
-                <div class="form-group bmd-form-group">
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text">
-                        <i class="material-icons">mail</i>
-                      </span>
-                    </div>
-                    <input type="text" class="form-control" placeholder="Email..." autocomplete="on">
-                  </div>
-                </div>
-                <div class="form-group bmd-form-group">
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text">
-                        <i class="material-icons">lock_outline</i>
-                      </span>
-                    </div>
-                    <input type="password" placeholder="Password..." class="form-control" autocomplete="on">
-                  </div>
-                </div>
+              <div id="collapse">
+      <div class="row">
+        <div class="col-md-12">
+          <div id="accordion" role="tablist">
+            <div class="card card-collapse">
+              <div class="card-header" role="tab" id="headingOne">
+                <h5 class="mb-0">
+                <a data-toggle="collapse" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne" class="">
+                Collapsible Group Item #1
+                <i class="material-icons">keyboard_arrow_down</i>
+                </a>
+                </h5>
               </div>
-            </form>
-          </div>
-          <div class="modal-footer justify-content-center">
-            <a href="#pablo" class="btn btn-warning btn-link btn-wd btn-lg">Get Started</a>
+              <div id="collapseOne" class="collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion" style="">
+                <div class="card-body">
+                  <div class="form-group bmd-form-group">
+                    <div class="input-group">
+                      <div class="input-group-prepend">
+                        <span class="input-group-text">
+                          <i class="material-icons">face</i>
+                        </span>
+                      </div>
+                      <input type="text" class="form-control" placeholder="First Name..." autocomplete="on">
+                    </div>
+                  </div>
+                  <div class="form-group bmd-form-group">
+                    <div class="input-group">
+                      <div class="input-group-prepend">
+                        <span class="input-group-text">
+                        <i class="material-icons">mail</i>
+                        </span>
+                      </div>
+                        <input type="text" class="form-control" placeholder="Email..." autocomplete="on">
+                    </div>
+                  </div>
+                  <div class="form-group bmd-form-group">
+                    <div class="input-group">
+                      <div class="input-group-prepend">
+                        <span class="input-group-text">
+                          <i class="material-icons">lock_outline</i>
+                        </span>
+                      </div>
+                      <input type="password" placeholder="Password..." class="form-control" autocomplete="on">
+                    </div>
+                  </div>
+                </div>
+                </div>
+                <div class="card card-collapse">
+                  <div class="card-header" role="tab" id="headingTwo">
+                    <h5 class="mb-0">
+                      <a class="collapsed" data-toggle="collapse" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        Collapsible Group Item #2
+                        <i class="material-icons">keyboard_arrow_down</i>
+                      </a>
+                    </h5>
+                  </div>
+                  <div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo" data-parent="#accordion">
+                    <div class="card-body">
+                      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    </div>
+                  </div>
+                </div>
+                <div class="card card-collapse">
+                  <div class="card-header" role="tab" id="headingThree">
+                    <h5 class="mb-0">
+                      <a class="collapsed" data-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        Collapsible Group Item #3
+                        <i class="material-icons">keyboard_arrow_down</i>
+                      </a>
+                    </h5>
+                  </div>
+                  <div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#accordion">
+                    <div class="card-body">
+                      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    </div>
+                  </div>
+                </div>
+            </div>
           </div>
         </div>
+      </div>
       </div>
     </div>
 
