aebee4561c9a8c77cba89b3be66abf175477a9a8
Daniel Medeiros <danielmmf@gmail.com>
2018-12-21 00:45:20 -0200

    Loader do pagamento

--- resources/views/client/readqr.php
+++ resources/views/client/readqr.php
@@ -116,7 +116,9 @@
                     <h6 class="card-category text-gray">{{usuario.displayName}}</h6>
                     <h4 class="card-title">{{usuario.email}}</h4>
                     <p class="card-description">
-                      {{usuario}}</p>
+                    	<video muted autoplay playsinline id="qr-video" width="200px"></video>
+    					<canvas id="debug-canvas"></canvas>
+    				</p>
                     <a href="#" class="btn btn-info btn-round">qual unidade ?</a>
                   </div>
                 </div>
@@ -297,6 +299,62 @@
        var token =  localStorage.getItem("access_token");
        console.log(usuario);
 	});
+</script>
+<script type="module">
+    import QrScanner from "../qr-scanner.min.js";
+    const video = document.getElementById('qr-video');
+    const debugCheckbox = document.getElementById('debug-checkbox');
+    const debugCanvas = document.getElementById('debug-canvas');
+    const debugCanvasContext = debugCanvas.getContext('2d');
+    const camQrResult = document.getElementById('cam-qr-result');
+    const fileSelector = document.getElementById('file-selector');
+    const fileQrResult = document.getElementById('file-qr-result');
+
+    function setResult(label, result) {
+        label.textContent = result;
+        alert(result);
+        
+        label.style.color = 'teal';
+        clearTimeout(label.highlightTimeout);
+        label.highlightTimeout = setTimeout(() => label.style.color = 'inherit', 100);
+    }
+
+    // ####### Web Cam Scanning #######
+
+    const scanner = new QrScanner(video, result => setResult(camQrResult, result));
+    scanner.start();
+
+
+    // ####### debug mode related code #######
+
+    //debugCheckbox.addEventListener('change', () => setDebugMode(debugCheckbox.checked));
+
+    function setDebugMode(isDebug) {
+        const worker = scanner._qrWorker;
+        worker.postMessage({
+            type: 'setDebug',
+            data: isDebug
+        });
+        if (isDebug) {
+            debugCanvas.style.display = 'block';
+            worker.addEventListener('message', handleDebugImage);
+        } else {
+            debugCanvas.style.display = 'none';
+            worker.removeEventListener('message', handleDebugImage);
+        }
+    }
+
+    function handleDebugImage(event) {
+        const type = event.data.type;
+        if (type === 'debugImage') {
+            const imageData = event.data.data;
+            if (debugCanvas.width !== imageData.width || debugCanvas.height !== imageData.height) {
+                debugCanvas.width = imageData.width;
+                debugCanvas.height = imageData.height;
+            }
+            debugCanvasContext.putImageData(imageData, 0, 0);
+        }
+    }
 </script>
   <noscript>
     <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=111649226022273&ev=PageView&noscript=1" />
