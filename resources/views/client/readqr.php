90312e5f729597602267518d8c4d60398342ddf4
Daniel Medeiros <danielmmf@gmail.com>
2018-12-21 00:46:56 -0200

    Loader do pagamento

--- resources/views/client/readqr.php
+++ resources/views/client/readqr.php
@@ -83,12 +83,10 @@
     <div class="container">
       <div class="row">
         <div class="col-md-8 ml-auto mr-auto text-center">
-          <h1 class="title">Faça seu login com o facebook </h1>
-          <br>
-          <a href="#" ng-click="login_facebook()" class="btn btn-social btn-fill btn-facebook">
-            <i class="fa fa-facebook-square"></i>   Logar com o Facebook
-          </a>
-          <div class="ripple-container"></div>
+          <h1 class="title">Aponte para o Qr code da comanda</h1>
+          
+         
+          
         </div>
       </div>
     </div>
