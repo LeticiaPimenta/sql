7697dc571773e6cb96fac47f11495a15477ce521
Eduardo Vogel <11eduardo.vogel@gmail.com>
2019-05-30 03:16:37 -0300

    benamigos

--- app/Classes/BenAmigo.php
+++ app/Classes/BenAmigo.php
@@ -22,7 +22,21 @@ class BenAmigo
       $eus = \App\BenAmigo::where('eu',$codigo)->get()->toArray();
       $amigos = \App\BenAmigo::where('amigo',$codigo)->get()->toArray(); 
 
+      $parents = \App\User::where('parent_user',$codigo)->get()->toArray(); 
+
       $meus_bens = array();
+
+
+      foreach ($parents as $parent) {
+        
+        $meus_bens[] = array(
+          'nome' => $parent['name'],
+          'email' => $parent['email'],
+          'user_token' => $parent['user_token'] 
+        );
+
+      }
+
       //$result = array_merge($eus, $amigos);
       foreach ($eus as $eu) {
       	$eu_ben_amigo = \App\User::where('user_token',$eu['amigo'])->first()->toArray();
