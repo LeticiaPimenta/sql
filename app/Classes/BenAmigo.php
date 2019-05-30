5f3a353ca86aafd99a400937f050c6deff1513a9
Eduardo Vogel <11eduardo.vogel@gmail.com>
2019-05-30 03:26:44 -0300

    benamigos

--- app/Classes/BenAmigo.php
+++ app/Classes/BenAmigo.php
@@ -23,8 +23,29 @@ class BenAmigo
       $amigos = \App\BenAmigo::where('amigo',$codigo)->get()->toArray(); 
 
       $parents = \App\User::where('parent_user',$codigo)->get()->toArray(); 
+      $meu_user = \App\User::where('user_token',$codigo)->first()->toArray(); 
+
+
 
       $meus_bens = array();
+
+      if($meu_user['parent_user'] !=""){
+        try {
+          if($meu_ben_amigo = \App\User::where('user_token',$meu_user['parent_user'])->first()){
+            $meu_ben_amigo = $meu_ben_amigo->toArray();
+            $meus_bens[] = array(
+              'nome' => $meu_ben_amigo['name'],
+              'email' => $meu_ben_amigo['email'],
+              'user_token' => $meu_ben_amigo['user_token'] 
+            );
+          }
+          
+        } catch (Exception $e) {
+          print_r($e->getMessage());
+        }
+      
+
+      }
 
 
       foreach ($parents as $parent) {
